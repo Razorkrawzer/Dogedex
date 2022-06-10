58ce89a934cba0862c77fd5990ce366ed7d40e6d
Jcrespo <jcrespo@likenuuk.com>
2022-06-10 01:38:00 -0500

    added camera permission, and camera preview

--- app/src/main/java/com/george/dogedex/MainActivity.kt
+++ app/src/main/java/com/george/dogedex/MainActivity.kt
@@ -1,8 +1,19 @@
 package com.george.dogedex
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Build.VERSION_CODES.M
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.george.dogedex.api.ApiServiceInterceptor
 import com.george.dogedex.auth.LoginActivity
 import com.george.dogedex.databinding.ActivityMainBinding
@@ -10,16 +21,31 @@ import com.george.dogedex.doglist.DogListActivity
 import com.george.dogedex.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "You need to accept camera permission to use the camera",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val user = User.getLoggedInUser(this)
         if (user == null) {
             openLoginActivity()
             return
-        }else{
+        } else {
             ApiServiceInterceptor.setSessionToken(user.authenticationToken)
         }
 
@@ -30,6 +56,61 @@ class MainActivity : AppCompatActivity() {
         binding.dogListFab.setOnClickListener {
             openDogListActivity()
         }
+
+        requestCameraPermission()
+    }
+
+    private fun requestCameraPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    startCamera()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                    AlertDialog.Builder(this)
+                        .setTitle("Aceptame por favor")
+                        .setMessage("Para poder acceder a tomar fotos a los perros es necesario que permitas el acceso a la camara")
+                        .setPositiveButton(android.R.string.ok) { _, _ ->
+                            requestPermissionLauncher.launch(
+                                Manifest.permission.CAMERA
+                            )
+                        }
+                        .setNegativeButton(android.R.string.cancel) { _, _ ->
+
+                        }.show()
+                }
+                else -> {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA)
+                }
+            }
+        } else {
+            startCamera()
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProvideFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProvideFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider = cameraProvideFuture.get()
+            // Preview
+            val preview = Preview.Builder().build()
+            preview.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
+
+            // Select back camera as default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            // Bind use cases to camera
+            cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview
+            )
+
+        }, ContextCompat.getMainExecutor(this))
     }
 
     private fun openDogListActivity() {
